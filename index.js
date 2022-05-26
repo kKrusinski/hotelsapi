97fd2f611e10532cb3309ff663c6862508fa0ec9
kKrusinski <kacper.krusinski@microsoft.wsei.edu.pl>
2022-05-26 11:59:54 +0200

    dbConnection

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+    ).then(()=>console.log("DBConnection is successsfull."))
+     .catch((error)=>{
+         console.log(error);
+     });
+     
+app.use(express.json());
+
+app.listen(process.env.PORT , ()=>{
+    console.log(`Server is running and listening at port ${process.env.PORT}.`);
+})
