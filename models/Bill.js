41da1297cb5a7b0da92b6cb0f7a3a48caa90b81b
kKrusinski <kacper.krusinski@microsoft.wsei.edu.pl>
2022-05-26 12:08:13 +0200

    Models: User, Room, Hotel, Bill

---
+++ models/Bill.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const BillSchema = new mongoose.Schema(
+    {
+        userId:{type:String, required:true},
+        hotelId:[{
+            rooms:[{
+                roomId:{
+                    type:String
+                },
+                howManyDays:{
+                    type:Number,
+                    default:1,
+                }
+            }
+        ],
+        }], 
+      amount:{type:Number, required:true},
+      discount:{type:Number}
+    },
+    {timestamps:true}
+)
+module.exports = mongoose.model("Bill", BillSchema); 
---
+++ models/Hotel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+
+const HotelSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    type:{
+        type: String,
+        required: true
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    rating:{
+        type: Number,
+        min:0,
+        max:5,
+    },
+    rooms:{
+        type: [String],
+    },
+    cheapestPrice:{
+        type: Number,
+        required: true
+    },
+    featured: {
+        type:Boolean,
+        deafult:false,
+    },
+});
+
+module.exports = mongoose.model("Hotel", HotelSchema);
